1c695419baae94777c3d1bcc3ed0ab87262412d1
bsalman <bsalman395850@gmail.com>
2025-03-10 15:26:14 +0100

    delet css fixer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,15 +1,18 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     //attributes
     #[Route('/', name: 'homepage')]
     public function homepage(): Response
     {
-        return new Response('<h1>Welcome to the homepage</h1>');
+        // return new Response('<h1>Welcome to the homepage</h1>');
+
+        return $this->render('main/homepage.html.twig');
     }
 }
